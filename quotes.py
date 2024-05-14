44f9ce877c53b620bf6fcffdaa2b4d4d1e7f08fe
Bink <91280973+Bink-lab@users.noreply.github.com>
2024-05-14 21:21:47 +0200

    Update quotes.py

--- quotes.py
+++ quotes.py
@@ -1,13 +1,8 @@
-import os
-import sys
-import socket
-import random
-
 from flask import Flask, jsonify
+import random
 
 app = Flask(__name__)
 
-# Function to get a random quote
 def get_random_quote():
     # Read quotes from the text file
     with open('quotes.txt', 'r') as file:
@@ -21,21 +16,10 @@ def get_random_quote():
 
     return random_quote
 
-@app.route("/")
-def hello_world():
-    version = sys.version_info
-    res = (
-        "<h1>Hello my friends</h1>"
-        f"<h2>{os.getenv('ENV')}</h2></br>"
-        f"Running Python: {version.major}.{version.minor}.{version.micro}<br>"
-        f"Hostname: {socket.gethostname()}"
-    )
-    return res
-
-@app.route("/random-quote")
+@app.route('/random-quote')
 def random_quote():
     quote = get_random_quote()
     return jsonify({'quote': quote})
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(debug=True)
